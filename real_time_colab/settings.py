1dac744274445ae281e047075553d5a9165b84fe
GEINOTECH <geinotech@gmail.com>
2025-03-04 22:25:59 +0100

    new update

--- real_time_colab/settings.py
+++ real_time_colab/settings.py
@@ -15,7 +15,6 @@ ALLOWED_HOSTS = ['*']  # Update with your domain in production
 
 # Application definition
 INSTALLED_APPS = [
-    'daphne'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
